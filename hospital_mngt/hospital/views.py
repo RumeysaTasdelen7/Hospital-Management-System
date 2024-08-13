5e93802f3e53e28f5112b61b38c3f348cef20770
RumeysaTasdelen7 <rumeysatasdelen06@gmail.com>
2024-08-13 19:44:55 +0300

    13.08.2024

---
+++ hospital_mngt/hospital/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def  About(request):
+    return render(request, 'about.html')
+
+def Home(request):
+    return render(request, 'home.html')
+
+def Contact(request):
+    return render(request, 'contact.html')
