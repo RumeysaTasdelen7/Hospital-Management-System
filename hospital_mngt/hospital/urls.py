c2d103d4b25fbc5e7a3a28f7a8aafa5153fcbb8b
RumeysaTasdelen7 <rumeysatasdelen06@gmail.com>
2024-08-23 23:34:27 +0300

    23.08.2024

--- hospital_mngt/hospital/urls.py
+++ hospital_mngt/hospital/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import About, Home, Contact
+from .views import About, Home, Contact, Login, Logout_admin
 
 urlpatterns = [
     path('', Home, name='home'),
     path('about/', About, name='about'),
     path('contact/', Contact, name='contact'),
+    path('admin_login/', Login, name='login'),
+    path('logout/', Logout_admin, name='logout_admin'),
 ]
--- hospital_mngt/hospital/views.py
+++ hospital_mngt/hospital/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 
 def  About(request):
     return render(request, 'about.html')
@@ -8,3 +11,35 @@ def Home(request):
 
 def Contact(request):
     return render(request, 'contact.html')
+
+
+def Index(request):
+    if not request.user.is_staff:
+        return redirect ('login')
+    return render(request, 'index.html')
+
+def Login(reguest):
+    error = ""
+    if reguest.method == "POST":
+        u = reguest.POST['uname']
+        p = reguest.POST['pwd']
+        user = authenticate(username=u, password=p)
+        try:
+            if user.is_staff:
+                Login(reguest, user)
+                error = "No"
+
+            else:
+                error = "yes"
+        except:
+            error = "yes"
+    d = {'error': error}
+    return render(reguest, 'login.html', d)
+
+
+def Logout_admin(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    
+    logout(request)
+    return redirect('login')
